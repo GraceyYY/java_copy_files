69a80d277270de307f718229388627eb3b5a036a
Grace <lunay0325@gmail.com>
2019-03-03 02:14:26 +0800

    Create static methods for copy files and directories in Copy class

---
+++ src/Copy.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class Copy {
+    public static void copyFilesUsingFileStreams(String srcPath, String destPath ){
+        File src = new File(srcPath);
+        File dest = new File(destPath);
+    }
+
+    private static void copyFile(File src, File dest) {
+
+    }
+
+    private static void copyDir(File src, File dest) {
+
+    }
+}
