32fd906450adc6057a36697856ac007e6faf1df7
Grace <lunay0325@gmail.com>
2019-03-03 02:29:45 +0800

    Implement the copyFile method

--- src/Copy.java
+++ src/Copy.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 
 public class Copy {
     public static void copyFilesUsingFileStreams(String srcPath, String destPath ){
@@ -7,7 +7,16 @@ public class Copy {
     }
 
     private static void copyFile(File src, File dest) {
-
+        try (InputStream input = new FileInputStream(src);
+             OutputStream output = new FileOutputStream(dest)) {
+            int byteRead;
+            byte[] buffer = new byte[1024];
+            while((byteRead = input.read(buffer)) != -1){
+                output.write(buffer, 0, byteRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void copyDir(File src, File dest) {
