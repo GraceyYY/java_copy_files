97f6a23973e020d2494015a090d438e4c957d722
Grace <lunay0325@gmail.com>
2019-03-03 03:28:52 +0800

    Add a test for copyFilesUsingFileStreams method

---
+++ src/Test.java
@@ -0,0 +1,5 @@
+public class Test {
+    public static void main(String[] args) {
+        Copy.copyFilesUsingFileStreams("/Users/luyang/Desktop/files", "/Users/luyang/Desktop/files_copied");
+    }
+}
