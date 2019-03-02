8dc87823364a69a7ee5868c2ef8d7662cd1ed657
Grace <lunay0325@gmail.com>
2019-03-03 03:02:03 +0800

    Implement the copyFilesUsingFileStream method

--- src/Copy.java
+++ src/Copy.java
@@ -1,10 +1,19 @@
 import java.io.*;
 
 public class Copy {
-    public static void copyFilesUsingFileStreams(String srcPath, String destPath ){
+    public static void copyFilesUsingFileStreams(String srcPath, String destPath) {
         File src = new File(srcPath);
         File dest = new File(destPath);
-        copyDir(src, dest);
+        try {
+            if (src.exists()) {
+                copyDir(src, dest);
+                System.out.println("Files has been copied to \"" + dest.getCanonicalPath() + "\"");
+            } else {
+                System.out.println("The path does not exist, please check again");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void copyFile(File src, File dest) {
@@ -12,7 +21,7 @@ public class Copy {
              OutputStream output = new FileOutputStream(dest)) {
             int byteRead;
             byte[] buffer = new byte[1024];
-            while((byteRead = input.read(buffer)) != -1){
+            while ((byteRead = input.read(buffer)) != -1) {
                 output.write(buffer, 0, byteRead);
             }
         } catch (IOException e) {
